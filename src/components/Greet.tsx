bae3c4191fe4c3613e497395379e44df7285f40e
joankirui <joankirui99@gmail.com>
2024-08-16 18:37:23 +0300

    initial commit with useReducer hook and how to use it

---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+// Use types when building applications and interfaces when building libraries
+type GreetProps = {
+  name: string;
+  messageCount?: number;
+  isLoggedIn: boolean;
+};
+
+export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name} You have ${props.messageCount} unread messages`
+          : `Welcome Guest`}
+      </h2>
+    </div>
+  );
+};
