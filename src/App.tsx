ab943c7331d6a214f6ffc49c35dbf71f84870a07
joankirui <joankirui99@gmail.com>
2024-09-13 18:35:55 +0300

    add how to use useRef,class component ,component prop and generic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,82 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Button } from './components/Button';
-import { Input } from './components/Input';
-import { Container } from './components/Container';
-import { ThemeContextProvider } from './components/context/ThemeContent';
-import { Box } from './components/context/Box';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { Container } from "./components/Container";
+import { ThemeContextProvider } from "./components/context/ThemeContent";
+import { Box } from "./components/context/Box";
+import { UserContextProvider } from "./components/context/UserContext";
+import { User } from "./components/context/User";
+import { Counter } from "./components/class/Counter";
+import { Private } from "./components/auth/Private";
+import { Profile } from "./components/auth/Profile";
+import { List } from "./components/generics/List";
 
 function App() {
   const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
+    first: "Bruce",
+    last: "Wayne",
+  };
 
   const nameList = [
     {
-      first: 'Bruce',
-      last: 'Wayne'
+      first: "Bruce",
+      last: "Wayne",
     },
     {
-      first: 'Clark',
-      last: 'Kent'
+      first: "Clark",
+      last: "Kent",
     },
     {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
+      first: "Princess",
+      last: "Diana",
+    },
+  ];
 
   return (
     <div className="App">
-      <Greet name='JoanKirui' isLoggedIn={false} />
+      <Greet name="JoanKirui" isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
-      <Status status='error'/>
+      <Status status="error" />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Leornado Dicaprio</Heading>
       </Oscar>
-      <Button handleClick={(event, id) => {
-        console.log('Button clicked', event, id)
-      }}
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
       />
-      <Input value='' handleChange={event => console.log(event)} />
-      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
+      <Counter message="The message is " />
+      <Private isLoggedIn={true} component={Profile} />
+      <List
+        items={[{
+          id: 1,
+          first: 'Bruce',
+          last: 'Wayne',
+        },
+        {
+          id: 2,
+          first: 'Joan',
+          last: 'Kirui',
+        },
+      ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
